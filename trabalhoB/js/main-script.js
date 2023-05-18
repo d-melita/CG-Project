5f1f596f8e2a8ce8fb3f11f33bf4097801447921
João Rocha <joao.tfrocha@protonmail.com>
2023-05-18 18:20:29 +0100

    refactor: change function names; add macros

--- trabalhoB/js/main-script.js
+++ trabalhoB/js/main-script.js
@@ -9,8 +9,24 @@
 /* GLOBAL VARIABLES */
 //////////////////////
 var camera, scene, renderer;
-
 var trailer, transformer, inferiorBody, superiorBody, leftArm, rightArm, head, feet;
+
+const WHITE = 0xffffff, BLACK = 0x000000, BLUE = 0x004bc4, RED = 0xff0000, GREY = 0x909090, BACKGROUND_COLOR = 0xccf7ff;
+
+const WHEEL_RADIUS = 1.5, WHEEL_HEIGHT = 1;
+const X_TIGHT = 1, Y_TIGHT = 3, Z_TIGHT = 1;
+const X_LEG = 3, Y_LEG = 10, Z_LEG = 2;
+const BACK_WHEEL_OFFSET = -3.5, MIDDLE_WHEEL_OFFSET = 0.5;
+const X_FOOT = 3, Y_FOOT = 2, Z_FOOT = 2;
+const X_ABDOMEN = 4, Y_ABDOMEN = 1, Z_ABDOMEN = 4;
+const X_CHEST = 8, Y_CHEST = 6, Z_CHEST = 4;
+const X_WAIST = 8, Y_WAIST = 2, Z_WAIST = 1;
+const X_ARM = 3, Y_ARM = 6, Z_ARM = 2;
+const X_FOREARM = 2, Y_FOREARM = 1, Z_FOREARM = 6;
+const ESCAPE_RADIUS = 0.5, ESCAPE_HEIGHT = 4, ESCAPE_OFFSET = 2;
+const X_HEAD = 2, Y_HEAD = 2, Z_HEAD = 2;
+const EYE_RADIUS = 0.25;
+const X_ANTENNA = 0.5, Y_ANTENNA = 0.5, Z_ANTENNA = 0.5;
 
 ////////////////////////////////
 /* INITIALIZE ANIMATION CYCLE */
@@ -70,10 +86,10 @@ function createScene() {
 
     scene = new THREE.Scene();
     scene.add(new THREE.AxisHelper(20));
-    scene.background = new THREE.Color(0xccf7ff);
+    scene.background = new THREE.Color(BACKGROUND_COLOR);
 
-    createTrailer(0, 0, -10);
-    createTransformer(0, 0, 0);
+    addTrailer(0, 0, -10);
+    addTransformer(0, 0, 0);
 }
 
 //////////////////////
@@ -124,106 +140,156 @@ function switchCamera(new_camera) {
 /* CREATE OBJECT3D(S) */
 ////////////////////////
 
-function createBox(obj, x, y, z, length, height, width, color) {
+function addBox(obj, x, y, z, length, height, width, color) {
     'use strict';
 
     var geometry = new THREE.BoxGeometry(length, height, width);
-    var material = new THREE.MeshBasicMaterial({ color: color, wireframe: false });
+    var material = new THREE.MeshBasicMaterial({color: color, wireframe: false});
     var box = new THREE.Mesh(geometry, material);
+
     box.position.set(x, y, z);
     obj.add(box);
 }
 
-function addWheel(obj, x, y, z, radius, height) {
+function addWheel(obj, x, y, z) {
     'use strict';
 
-    var geometry = new THREE.CylinderGeometry(radius, radius, height, 32);
+    var geometry = new THREE.CylinderGeometry(
+      WHEEL_RADIUS, WHEEL_RADIUS, WHEEL_HEIGHT, 32
+    );
     geometry.rotateZ(Math.PI/2);
-    var material = new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: false });
+    var material = new THREE.MeshBasicMaterial({color: BLACK, wireframe: false});
     var wheel = new THREE.Mesh(geometry, material);
+
     wheel.position.set(x, y, z);
     obj.add(wheel);
 }
 
-function createTrailer(x, y, z) {
+function addFeet(obj) {
+    'use strict';
+
+    feet = new THREE.Object3D();
+
+    addBox(feet, X_FOOT/2, Y_FOOT/2, Z_FOOT/2, X_FOOT, Y_FOOT, Z_FOOT, BLUE);
+    addBox(feet, -X_FOOT/2, Y_FOOT/2, Z_FOOT/2, X_FOOT, Y_FOOT, Z_FOOT, BLUE);
+
+    feet.position.set(0, -Y_LEG/2, Z_LEG/2);
+    obj.add(feet);
+}
+
+function addLeg(obj, left) {
+    'use strict';
+
+    var leg = new THREE.Object3D();
+
+      addBox(leg, 0, 0, 0, X_LEG, Y_LEG, Z_LEG, BLUE);
+    if (left) {
+      addWheel(leg, X_LEG/2 + WHEEL_HEIGHT/2, -3.5, WHEEL_RADIUS - Z_LEG/2);
+      addWheel(leg, X_LEG/2 + WHEEL_HEIGHT/2, 0.5, WHEEL_RADIUS - Z_LEG/2);
+
+      leg.position.set(X_LEG/2, 0, 0);
+    } else {
+      addWheel(leg, - X_LEG/2 - WHEEL_HEIGHT/2, -3.5, WHEEL_RADIUS - Z_LEG/2);
+      addWheel(leg, - X_LEG/2 - WHEEL_HEIGHT/2, 0.5, WHEEL_RADIUS - Z_LEG/2);
+
+      leg.position.set(-X_LEG/2, 0, 0);
+    }
+
+    obj.add(leg);
+}
+
+function addLegs(obj) {
+    'use strict';
+
+    var legs = new THREE.Object3D();
+
+    addLeg(legs, true);
+    addLeg(legs, false);
+
+    obj.add(legs);
+}
+
+function addLegsAndFeet(obj) {
+    'use strict';
+
+    var legsAndFeet = new THREE.Object3D();
+
+    addLegs(legsAndFeet);
+    addFeet(legsAndFeet);
+
+    legsAndFeet.position.set(0, - Y_TIGHT - Y_LEG/2, Z_LEG/2);
+    obj.add(legsAndFeet);
+}
+
+function addTights(obj) {
+    'use strict';
+
+    var tights = new THREE.Object3D();
+
+    addBox(tights, -X_TIGHT/2, 0, 0, X_TIGHT, Y_TIGHT, Z_TIGHT, GREY);
+    addBox(tights, X_TIGHT/2, 0, 0, X_TIGHT, Y_TIGHT, Z_TIGHT, GREY);
+
+    tights.position.set(0, -Y_TIGHT/2, Z_TIGHT/2);
+    obj.add(tights);
+}
+
+function addInferiorBody(obj) {
+    'use strict';
+
+    inferiorBody = new THREE.Object3D();
+
+    addTights(inferiorBody);
+    addLegsAndFeet(inferiorBody);
+
+    inferiorBody.position.set(0, Y_LEG + Y_TIGHT, 0);
+    obj.add(inferiorBody);
+}
+
+function addAbdomen() {}
+
+function addSuperiorBody(obj) {
+    'use strict';
+
+    superiorBody = new THREE.Object3D();
+
+    addAbdomen();
+
+    superiorBody.position.set(0, Y_LEG + Y_TIGHT, 0);
+    obj.add(superiorBody);
+}
+
+function addTransformer(x, y, z) {
+    'use strict';
+
+    transformer = new THREE.Object3D();
+
+    addInferiorBody(transformer);
+    addSuperiorBody(transformer);
+
+    transformer.position.set(x, y, z);
+    scene.add(transformer);
+}
+
+function addTrailer(x, y, z) {
     'use strict';
 
     trailer = new THREE.Object3D();
 
-    createBox(trailer, 0, 6.5, 12, 8, 5, 24, 0xffffff); // trailer top
-    createBox(trailer, 0, 2.5, 19, 6, 3, 10, 0xa8a8a8); // trailer base
+    addBox(trailer, 0, 6.5, -12, 8, 5, 24, WHITE); // trailer top
+    addBox(trailer, 0, 2.5, -19, 6, 3, 10, GREY); // trailer base
 
-    addWheel(trailer, -3.5, 1.5, 17.5, 1.5, 1);
-    addWheel(trailer, 3.5, 1.5, 17.5, 1.5, 1);
-    addWheel(trailer, -3.5, 1.5, 21.5, 1.5, 1);
-    addWheel(trailer, 3.5, 1.5, 21.5, 1.5, 1);
+    addWheel(trailer, -3.5, 1.5, -17.5);
+    addWheel(trailer, 3.5, 1.5, -17.5);
+    addWheel(trailer, -3.5, 1.5, -21.5);
+    addWheel(trailer, 3.5, 1.5, -21.5);
 
-    createBox(trailer, 0, 3.5, 0.5, 2, 1, 1, 0x000000); // trailer hitch
+    addBox(trailer, 0, 3.5, -0.5, 2, 1, 1, BLACK); // trailer hitch
 
-    trailer.rotateY(Math.PI);
-    
     scene.add(trailer);
 
     trailer.position.x = x;
     trailer.position.y = y;
     trailer.position.z = z;
-}
-
-function createLegs(obj) {
-    'use strict';
-
-    var legs = new THREE.Object3D();
-
-    createBox(inferiorBody, -1.5, 5, 1, 3, 10, 2, 0x004bc4); //left leg
-    createBox(inferiorBody, 1.5, 5, 1, 3, 10, 2, 0x004bc4); // right leg
-
-    addWheel(legs, -3.5, 1.5, 1.5, 1.5, 1.5, 1); // left bottom wheel
-    addWheel(legs, -3.5, 5.5, 1.5, 1.5, 1.5, 1); // left top wheel
-    addWheel(legs, 3.5, 1.5, 1.5, 1.5, 1.5, 1); // right bottom wheel
-    addWheel(legs, 3.5, 5.5, 1.5, 1.5, 1.5, 1); // right top wheel
-
-    createBox(legs, -0.5, 11.5, 0.5, 1, 3, 1, 0x000000); // left leg tight
-    createBox(legs, 0.5, 11.5, 0.5, 1, 3, 1, 0x000000); // right leg tight
-
-    obj.add(legs);
-}
-
-function createFeet(obj) {
-    'use strict';
-
-    feet = new THREE.Object3D();
-
-    createBox(inferiorBody, -1.5, 1, 3, 3, 2, 2, 0x004bc4); // left foot
-    createBox(inferiorBody, 1.5, 1, 3, 3, 2, 2, 0x004bc4); // right foot
-
-    obj.add(feet);
-}
-
-function createInferiorBody(obj) {
-    'use strict';
-
-    inferiorBody = new THREE.Object3D();
-
-    createLegs(inferiorBody);
-    createFeet(inferiorBody);
-
-    obj.add(inferiorBody);
-}
-
-function createSuperiorBody(obj) {}
-
-function createTransformer(x, y, z) {
-    'use strict';
-
-    transformer = new THREE.Object3D();
-
-    createInferiorBody(transformer);
-    createSuperiorBody(transformer);
-
-    scene.add(transformer);
-    transformer.position.x = x;
-    transformer.position.y = y;
-    transformer.position.z = z;
 }
 
 //////////////////////
