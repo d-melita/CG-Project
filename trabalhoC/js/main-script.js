2a30091492672d3f61da38a89ce25d3b5999f9fa
Diogo Melita <dgmelita@gmail.com>
2023-06-01 17:31:46 +0100

    add sky prototype

--- trabalhoC/js/main-script.js
+++ trabalhoC/js/main-script.js
@@ -121,11 +121,47 @@ function createScene() {
     axesHelper = new THREE.AxesHelper(20);
     scene.add(axesHelper);
     scene.background = new THREE.Color(BACKGROUND_COLOR);
+    createSky();
     addHouse();
     addOVNI(0, 20, 0);
     addMoon(0, 35, 15);
     addTree(0, 1.5, 30);
 }
+
+function createSky() {
+    const geometry = new THREE.BufferGeometry();
+  
+    const positions = [0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0].map(
+      (n) => n  * 20
+    );
+  
+    const indices = [0, 1, 2, 2, 3, 0];
+  
+    const blue = new THREE.Color(0x0000ff);
+    const purple = new THREE.Color(0xa32cc4);
+    const colors = [purple, blue, blue, purple].flatMap((color) => [
+      color.r,
+      color.g,
+      color.b,
+    ]);
+  
+    geometry.setIndex(indices);
+    geometry.setAttribute(
+      "position",
+      new THREE.Float32BufferAttribute(positions, 3)
+    );
+    geometry.computeVertexNormals();
+    geometry.setAttribute("color", new THREE.Float32BufferAttribute(colors, 3));
+  
+    const material = new THREE.MeshBasicMaterial({
+      vertexColors: true,
+    });
+  
+    const sky = new THREE.Mesh(geometry, material);
+    sky.position.set(0, 0, 0);
+  
+    scene.add(sky);
+  }
 
 //////////////////////
 /* CREATE CAMERA(S) */
